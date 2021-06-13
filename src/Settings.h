d51aacae4342d7021de6049f3509ab4d12086a36
Jakub Jaszczur <69448053+JakubJaszczur@users.noreply.github.com>
2021-06-13 17:25:23 +0200

    Add two sensors and send data to mqtt

--- src/Settings.h
+++ src/Settings.h
@@ -1,20 +1,18 @@
 // Misc
 #define DEVICE_NAME         "Temperature"
-#define UPDATE_INTERVAL     5
+#define DEV_ID              190
+#define UPDATE_INTERVAL     60      //seconds
+#define ONE_WIRE_BUS        2       //D4
 
 // MQTT
 #define MQTT_MAX_PACKET_SIZE 256
-#define LORA_TOPIC          "home/lora"
-#define HC12_TOPIC          "home/433"
-#define LORA_SEND_TOPIC     "gateway/send/lora"
-#define HC12_SEND_TOPIC     "gateway/send/433"
-#define SERVICE_TOPIC       "gateway/service"
-#define DEBUG_TOPIC         "gateway/debug"
+#define MQTT_TOPIC          "test/temperatures"
 
 // Sensors
 #define ALTITUDE        515.0 //define altitude of location
 #define BME280_ADDR     0x76  //BME280 I2C Address
 #define MCP9808_ADDR    0x18  //MCP9808 I2C Address
 #define SHT31_ADDR      0x44
+#define AHT10_ADDR      0x38
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,20 @@
 #include "Adafruit_MCP9808.h"
 #include <Adafruit_BME280.h>
 #include "Adafruit_SHT31.h"
+#include <Adafruit_AHTX0.h>
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 Adafruit_MCP9808 mcp = Adafruit_MCP9808();   // temp
 Adafruit_BME280 bme;                         // temp, hum, press
 Adafruit_SHT31 sht = Adafruit_SHT31();       // temp, hum
+Adafruit_AHTX0 aht;                          // temp, hum
 WiFiClient espClient;
 PubSubClient mqtt(espClient);
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature ds19(&oneWire);
 
 String version = String(VERSION_SHORT);
 
@@ -70,11 +78,10 @@ void callback(char* topic, byte* payload, unsigned int length)
   
   Serial.println(data);
 
-
-  if(String(topic) == HC12_SEND_TOPIC)
-  {
-
-  }
+  //if(String(topic) == "HC12_SEND_TOPIC")
+  //{
+  //
+  //}
 }
 
 void ConnectToMQTT() 
@@ -94,7 +101,7 @@ void ConnectToMQTT()
     {
       Serial.println("Connected");
 
-      mqtt.subscribe(SERVICE_TOPIC);
+      //mqtt.subscribe(SERVICE_TOPIC);
     } 
     else 
     {
@@ -116,22 +123,11 @@ bool CheckMqttConnection()
   return true;
 }
 
-void MqttSend(String data, bool hc12)
-{ 
-  CheckMqttConnection();
-
-  char message[200];
-  data.toCharArray(message, sizeof(message));
-
-  mqtt.publish(LORA_TOPIC, message);
-
-}
-
 // MCP9808 functions
 
 boolean InitialiseMPC9808(byte mode)
 {
-  if( mcp.begin(MCP9808_ADDR)) 
+  if(!mcp.begin(MCP9808_ADDR)) 
   {
     Serial.println("MCP9808 Error");
     return false;
@@ -156,20 +152,6 @@ float GetTemperatureMCP9808()
  mcp.shutdown_wake(1); // shutdown MSP9808 - power consumption ~0.1 mikro Ampere, stops temperature sampling
 
   return temperature;
-}
-
-String ComposeJSONmessage(int id, float temp, float hum, float press, float lux, float uv, int pm_25, int pm_10, float bat, float panel)
-{
-  String message;
-
-  const size_t capacity = JSON_OBJECT_SIZE(10) + 90;
-  DynamicJsonDocument doc(capacity);
-
-    doc["id"] = id;
-
-  serializeJson(doc,message);
-
-  return message;
 }
 
 // BME280 functions
@@ -219,7 +201,7 @@ float GetPressureBME280()
 
 boolean InitialiseSHT31()
 {
-  if (!sht.begin(SHT31_ADDR))
+  if(!sht.begin(SHT31_ADDR))
   //if (!bme.begin())  
   {
     Serial.println("SHT31 Error");
@@ -235,9 +217,90 @@ boolean InitialiseSHT31()
 
 float GetTemperatureSHT31()
 {
-  float temperature = sht.readTemperature();;
+  float temperature = sht.readTemperature();
   
   return temperature;
+}
+
+// SHT31 functions
+
+boolean InitialiseAHT10()
+{
+  if(!aht.begin(&Wire, AHT10_ADDR))
+  {
+    Serial.println("AHT10 Error");
+    return false;
+  }
+  else
+  {
+    Serial.println("AHT10 Initialised");
+    return true;
+  }
+}
+
+float GetTemperatureAHT10()
+{
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
+  float temperature = temp.temperature;
+  
+  return temperature;
+}
+
+// DS19B20 functions
+
+boolean InitialiseDS19B20()
+{
+  ds19.begin();
+
+  if(ds19.getTempCByIndex(0) == DEVICE_DISCONNECTED_C)
+  {
+    Serial.println("DS19B20 Error");
+    return false;
+  }
+  else
+  {
+    Serial.println("DS19B20 Initialised");
+    return true;
+  }
+}
+
+float GetTemperatureDS19B20()
+{
+  ds19.requestTemperatures();
+  float temperature = ds19.getTempCByIndex(0);
+  
+  return temperature;
+}
+
+String ComposeJSONmessage(float mpc, float bme, float sht, float aht, float ds)
+{
+  String message;
+
+  const size_t capacity = JSON_OBJECT_SIZE(6) + 60;
+  DynamicJsonDocument doc(capacity);
+
+    //doc["id"] = DEV_ID;
+    doc["mpc"] = mpc;
+    doc["bme"] = bme;
+    doc["sht"] = sht;
+    doc["aht"] = aht;
+    doc["ds"] = ds;
+
+  serializeJson(doc, message);
+
+  return message;
+}
+
+void MqttSend(String data)
+{ 
+  CheckMqttConnection();
+
+  char message[200];
+  data.toCharArray(message, sizeof(message));
+
+  mqtt.publish(MQTT_TOPIC, message);
+  Serial.println("Data sent to MQTT!");
 }
 
 // MAIN CODE //
@@ -250,9 +313,11 @@ void setup()
   ConnectToWifi();
   ConnectToMQTT();
 
-  InitialiseMPC9808(3);
+  InitialiseMPC9808(2);
   InitialiseBME280();
   InitialiseSHT31();
+  InitialiseAHT10();
+  InitialiseDS19B20();
 }
 
 void loop() 
@@ -262,19 +327,30 @@ void loop()
   float tempMPC9808 = 0;
   float tempBME280 = 0;
   float tempSHT31 = 0;
+  float tempAHT10 = 0;
+  float tempDS19B20 = 0;
 
   if(actualTime - lastUpdateTime > UPDATE_INTERVAL * 1000)
   {
     tempMPC9808 = GetTemperatureMCP9808();
     tempBME280 = GetTemperatureBME280();
     tempSHT31 = GetTemperatureSHT31();
+    tempAHT10 = GetTemperatureAHT10();
+    tempDS19B20 = GetTemperatureDS19B20();
 
-    Serial.println("MPC9808    BME280     SHT31");
+    Serial.println("MPC9808    BME280     SHT31     AHT10     DS19B20");
     Serial.print(tempMPC9808);
     Serial.print("      ");
     Serial.print(tempBME280);
     Serial.print("      ");
-    Serial.println(tempSHT31);
+    Serial.print(tempSHT31);
+    Serial.print("     ");
+    Serial.print(tempAHT10);
+    Serial.print("     ");
+    Serial.println(tempDS19B20);
+
+    String message = ComposeJSONmessage(tempMPC9808, tempBME280, tempSHT31, tempAHT10, tempDS19B20);
+    MqttSend(message);
 
     lastUpdateTime = actualTime;
   }
